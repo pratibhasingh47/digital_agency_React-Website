2b91e5d8d2e4bdc5b394d0df743c65e957aafc96
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-07-06 22:05:36 +0530

    jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,122 +1,115 @@
-import React from 'react';
-import './footer.css'
+import React from "react";
+import "./footer.css";
 
 const quickLinks01 = [
-	{
-		path: '#',
-		display: 'Marketing'
-	},
+    {
+        path: "#",
+        display: "Marketing",
+    },
 
-	{
-		path: '#',
-		display: 'Analytics'
-	},
+    {
+        path: "#",
+        display: "Analytics",
+    },
 
-	{
-		path: '#',
-		display: 'Commerce'
-	},
+    {
+        path: "#",
+        display: "Commerce",
+    },
 ];
-
 
 const quickLinks02 = [
-	{
-		path: '#',
-		display: 'Pricing'
-	},
+    {
+        path: "#",
+        display: "Pricing",
+    },
 
-	{
-		path: '#',
-		display: 'Documentation',
-	},
+    {
+        path: "#",
+        display: "Documentation",
+    },
 
-	{
-		path: '#',
-		display: 'Guides',
-	},
+    {
+        path: "#",
+        display: "Guides",
+    },
 ];
-
-
 
 const quickLinks03 = [
-	{
-		path: '#about',
-		display: 'About',
-	},
+    {
+        path: "#about",
+        display: "About",
+    },
 
-	{
-		path: '#',
-		display: 'Job',
-	},
+    {
+        path: "#",
+        display: "Job",
+    },
 
-	{
-		path: '#blog',
-		display: 'Blog',
-	},
+    {
+        path: "#blog",
+        display: "Blog",
+    },
 ];
 
-
 const Footer = () => {
+    const year = new Date().getFullYear();
 
-	const year = new Date().getFullYear()
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="footer__wrapper">
+                    <div className="footer__logo">
+                        <h2>Agency</h2>
+                        <p className="description">Grow with us</p>
+                        <p className="small__text description">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Temporibus, labore facilis hic dolore eveniet,
+                            dolor architecto asperiores laborum
+                        </p>
+                    </div>
 
+                    <div className="footer__quick-links">
+                        <h3 className="quick__links-title">Solutions</h3>
+                        <ul className="quick__links">
+                            {quickLinks01.map((item, index) => (
+                                <li key={index} className="quick__link-item">
+                                    <a href={item.path}>{item.display}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
 
-	return (
-		<footer className="footer">
-			<div className="container">
-				<div className="footer__wrapper">
-					<div className="footer__logo">
-						<h2>Agency</h2>
-						<p className="description">
-							Grow with us
-						</p>
-						<p className="small__text description">
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, labore facilis hic dolore eveniet, dolor architecto asperiores laborum
-						</p>
+                    <div className="footer__quick-links">
+                        <h3 className="quick__links-title">Support</h3>
+                        <ul className="quick__links">
+                            {quickLinks02.map((item, index) => (
+                                <li key={index} className="quick__link-item">
+                                    <a href={item.path}>{item.display}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
 
-					</div>
+                    <div className="footer__quick-links">
+                        <h3 className="quick__links-title">Company</h3>
+                        <ul className="quick__links">
+                            {quickLinks03.map((item, index) => (
+                                <li key={index} className="quick__link-item">
+                                    <a href={item.path}>{item.display}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
 
-					<div className="footer__quick-links">
-						<h3 className="quick__links-title">Solutions</h3>
-						<ul className="quick__links">
-							{
-								quickLinks01.map((item, index) => (
-									<li key={index} className='quick__link-item'><a href={item.path}>{item.display}</a></li>
-								))
-							}
-						</ul>
-					</div>
+                <p className="copyright">
+                    Copyright {year}, developed by Pratibha with ❤️. All rights
+                    reserved.{" "}
+                </p>
+            </div>
+        </footer>
+    );
+};
 
-
-					<div className="footer__quick-links">
-						<h3 className="quick__links-title">Support</h3>
-						<ul className="quick__links">
-							{
-								quickLinks02.map((item, index) => (
-									<li key={index} className='quick__link-item'><a href={item.path}>{item.display}</a></li>
-								))
-							}
-						</ul>
-					</div>
-
-
-					<div className="footer__quick-links">
-						<h3 className="quick__links-title">Company</h3>
-						<ul className="quick__links">
-							{
-								quickLinks03.map((item, index) => (
-									<li key={index} className='quick__link-item'><a href={item.path}>{item.display}</a></li>
-								))
-							}
-						</ul>
-					</div>
-				</div>
-
-				<p className="copyright">Copyright {year}, developed by Pratibha with ❤️. All rights reserved.{' '}
-				</p>
-			</div>
-		</footer>
-	)
-}
-
-export default Footer
+export default Footer;
