55bc105d101f2632869ecf32f7dfc728327274e6
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-07-06 22:08:31 +0530

    jsx

--- src/components/UI/Newsletter.jsx
+++ src/components/UI/Newsletter.jsx
@@ -1,31 +1,29 @@
-import React from 'react'
-import '../../styles/newsletter.css';
+import React from "react";
+import "../../styles/newsletter.css";
 
 const Newsletter = () => {
-	return (
-		<section className="newsletter">  
+    return (
+        <section className="newsletter">
+            <div className="container">
+                <div className="newsletter_wrapper">
+                    <div className="newsletter_content">
+                        <h6 className="subtitle">Let's Work</h6>
+                        <h2>
+                            Explore the{" "}
+                            <span className="highlight">hidden </span>ideas.
+                        </h2>
+                    </div>
 
-			<div className="container">
+                    <div className="newsletter_form">
+                        <input type="email" placeholder="Email" />
+                        <button className="secondary__btn subscribe_btn">
+                            Like Now
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
 
-				<div className="newsletter_wrapper">
-
-					<div className="newsletter_content">
-
-						<h6 className="subtitle">Let's Work</h6>
-						<h2>Explore the <span className="highlight">hidden </span>ideas.</h2>
-
-					</div>
-
-					<div className="newsletter_form">
-						<input type="email" placeholder='Email' />
-						<button className="secondary__btn subscribe_btn">Like Now</button>
-					</div>
-
-					
-				</div>
-			</div>
-		</section>
-	)
-}
-
-export default Newsletter
+export default Newsletter;
