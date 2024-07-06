f1348a7ef77c865035aeac76331cbbf698c02662
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-07-06 22:08:21 +0530

    jsx

--- src/components/UI/Hero.jsx
+++ src/components/UI/Hero.jsx
@@ -1,36 +1,50 @@
-'use client'
-import React from 'react'
-import '../../styles/hero.css';
-import heroDarkImg from '../images/hero-img.png'
-import lightImg from '../images/light-hero-bg.jpg';
+"use client";
+import React from "react";
+import "../../styles/hero.css";
+import heroDarkImg from "../images/hero-img.png";
+import lightImg from "../images/light-hero-bg.jpg";
 
 const Hero = (theme) => {
-	return (
-		<section className="hero__section" id='hero'>
-			<div className="container">
-				<div className="hero__wrapper">
-					<div className="hero__content">
-						<div>
-							<h2>We're Creating Perfect</h2>
-							<h2>Digital Products To</h2>
-							<h2 className='highlight'>Promote Your Brand</h2>
-						</div>
-						<p className="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam, accusamus vero soluta commodi aliquid magni adipisci maiores aperiam repellat quaerat nam! Odio veniam perferendis cumque corporis sunt necessitatibus nisi illum?</p>
+    return (
+        <section className="hero__section" id="hero">
+            <div className="container">
+                <div className="hero__wrapper">
+                    <div className="hero__content">
+                        <div>
+                            <h2>We're Creating Perfect</h2>
+                            <h2>Digital Products To</h2>
+                            <h2 className="highlight">Promote Your Brand</h2>
+                        </div>
+                        <p className="description">
+                            Lorem ipsum, dolor sit amet consectetur adipisicing
+                            elit. Aliquam, accusamus vero soluta commodi aliquid
+                            magni adipisci maiores aperiam repellat quaerat nam!
+                            Odio veniam perferendis cumque corporis sunt
+                            necessitatibus nisi illum?
+                        </p>
 
-						<div className="hero__btns">
-							<button className='primary__btn'>Get Started Now</button>
-							<button className='secondary__btn'>Discover More</button>
-						</div>
-					</div>
+                        <div className="hero__btns">
+                            <button className="primary__btn">
+                                Get Started Now
+                            </button>
+                            <button className="secondary__btn">
+                                Discover More
+                            </button>
+                        </div>
+                    </div>
 
-					<div className="hero__img">
-						<img src={theme === "light-theme" ? lightImg : heroDarkImg} alt="hero-img" />
-					</div>
+                    <div className="hero__img">
+                        <img
+                            src={
+                                theme === "light-theme" ? lightImg : heroDarkImg
+                            }
+                            alt="hero-img"
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
 
-				</div>
-			</div>
-		</section>
-	)
-}
-
-export default Hero
+export default Hero;
